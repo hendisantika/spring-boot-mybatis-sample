c515d0c7cba58e2ef632972e0bf7b98b5c423cd8
Hendi Santika <hendisantika@gmail.com>
2018-09-03 19:58:46 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootmybatissample/SpringBootMybatisSampleApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.springbootmybatissample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMybatisSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMybatisSampleApplication.class, args);
+    }
+}
